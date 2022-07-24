1582c153d604c3f8c256a62440a15e05a9e3b86d
aoba <ipbdipbd00@gmail.com>
2022-07-24 23:02:08 +0900

    Add reverse even

--- linked_list/linked_list.go
+++ linked_list/linked_list.go
@@ -2,36 +2,40 @@ package linkedlist
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"reflect"
+	"strings"
 )
 
-type Node[T comparable] struct {
-	data T
-	next *Node[T]
+type Node struct {
+	data int
+	next *Node
 }
 
-func NewNode[T comparable](data T) *Node[T] {
-	return &Node[T]{
+func NewNode(data int) *Node {
+	return &Node{
 		data: data,
 		next: nil,
 	}
 }
 
-type LinkedList[T comparable] struct {
-	head *Node[T]
+type LinkedList struct {
+	head *Node
 }
 
-func NewLinkedList[T comparable]() *LinkedList[T] {
-	return &LinkedList[T]{
+func NewLinkedList() *LinkedList {
+	return &LinkedList{
 		head: nil,
 	}
 }
 
-func (l *LinkedList[T]) Head() *Node[T] {
+func (l *LinkedList) Head() *Node {
 	return l.head
 }
 
 // Insert　最後尾にNodeを追加する
-func (l *LinkedList[T]) Append(data T) {
+func (l *LinkedList) Append(data int) {
 	newNode := NewNode(data)
 	if l.head == nil {
 		l.head = newNode
@@ -46,13 +50,13 @@ func (l *LinkedList[T]) Append(data T) {
 }
 
 // Insert　先頭にNodeを追加する
-func (l *LinkedList[T]) Insert(data T) {
+func (l *LinkedList) Insert(data int) {
 	newNode := NewNode(data)
 	newNode.next = l.head
 	l.head = newNode
 }
 
-func (l *LinkedList[T]) Remove(data T) {
+func (l *LinkedList) Remove(data int) {
 	currentNode := l.head
 	if currentNode != nil && currentNode.data == data {
 		l.head = currentNode.next
@@ -60,7 +64,7 @@ func (l *LinkedList[T]) Remove(data T) {
 		return
 	}
 
-	var previousNode *Node[T]
+	var previousNode *Node
 	for currentNode != nil && currentNode.data != data {
 		previousNode = currentNode
 		currentNode = currentNode.next
@@ -73,29 +77,87 @@ func (l *LinkedList[T]) Remove(data T) {
 	previousNode.next = currentNode.next
 }
 
-func (l *LinkedList[T]) Print() {
+func (l *LinkedList) Print(writer io.Writer) {
 	iterator := l.head
 	for iterator != nil {
-		fmt.Printf("%v ", iterator.data)
+		fmt.Fprintf(writer, "%v ", iterator.data)
 		iterator = iterator.next
 	}
-	fmt.Println()
+}
+
+func (l *LinkedList) ReverseIterative() {
+	var prevNode *Node
+	currentNode := l.head
+	for currentNode != nil {
+		nextNode := currentNode.next
+		currentNode.next = prevNode
+		prevNode = currentNode
+		currentNode = nextNode
+	}
+	l.head = prevNode
+}
+
+func (l *LinkedList) ReverseRecursive() {
+	l.head = reverseRecursiveHelper(nil, l.head)
+}
+
+func reverseRecursiveHelper(previous, current *Node) *Node {
+	if current == nil {
+		return previous
+	}
+
+	nextNode := current.next
+	current.next = previous
+	previous = current
+	current = nextNode
+
+	return reverseRecursiveHelper(previous, current)
+}
+
+func (l *LinkedList) ReverseEven() {
+	var prevNode *Node
+	currentNode := l.head
+	for !reflect.ValueOf(currentNode).IsNil() {
+		if currentNode.data%2 == 0 {
+			beforeNode := prevNode
+			startNode := currentNode
+			endNode, nextNode := reverseEvenHelper(currentNode)
+			beforeNode.next = endNode
+			startNode.next = nextNode
+			currentNode = nextNode
+			prevNode = startNode
+		} else {
+			prevNode = currentNode
+			currentNode = currentNode.next
+		}
+	}
+}
+
+func reverseEvenHelper(startNode *Node) (endNode *Node, nextNode *Node) {
+	prevNode := startNode
+	currentNode := startNode.next
+	for currentNode != nil && currentNode.data%2 == 0 {
+		nextNode := currentNode.next
+		currentNode.next = prevNode
+		prevNode = currentNode
+		currentNode = nextNode
+	}
+
+	return prevNode, currentNode
 }
 
 func Run() {
-	l := NewLinkedList[int]()
+	l := NewLinkedList()
+	l.Append(0)
 	l.Append(1)
 	l.Append(2)
 	l.Append(3)
 	l.Append(4)
-	l.Insert(0)
-	l.Print()
-	l.Remove(3)
-	l.Print()
-	l.Remove(0)
-	l.Print()
-	l.Remove(4)
-	l.Print()
-	l.Remove(100)
-	l.Print()
+
+	fmt.Println(strings.Repeat("#", 10))
+	l.Print(os.Stdout)
+	l.ReverseIterative()
+	l.Print(os.Stdout)
+	l.ReverseRecursive()
+	l.Print(os.Stdout)
 }
--- linked_list/linked_list_test.go
+++ linked_list/linked_list_test.go
@@ -1,11 +1,77 @@
-package linkedlist_test
+package linkedlist
 
 import (
+	"bytes"
+	"reflect"
 	"testing"
-
-	linkedlist "github.com/laster18/algo-go/linked_list"
 )
 
 func Test_Run(t *testing.T) {
-	linkedlist.Run()
+}
+
+func Test_ReverseEven(t *testing.T) {
+	tests := []struct {
+		name    string
+		prepare func() *LinkedList
+		want    string
+	}{
+		{
+			name: "Case1",
+			prepare: func() *LinkedList {
+				l := NewLinkedList()
+				l.Append(1)
+				l.Append(4)
+				l.Append(6)
+				l.Append(8)
+				l.Append(9)
+				return l
+			},
+			want: "1 8 6 4 9 ",
+		},
+		{
+			name: "Case2",
+			prepare: func() *LinkedList {
+				l := NewLinkedList()
+				l.Append(1)
+				l.Append(2)
+				l.Append(3)
+				l.Append(4)
+				l.Append(5)
+				l.Append(6)
+				return l
+			},
+			want: "1 2 3 4 5 6 ",
+		},
+		{
+			name: "Case2",
+			prepare: func() *LinkedList {
+				l := NewLinkedList()
+				l.Append(1)
+				l.Append(2)
+				l.Append(4)
+				l.Append(6)
+				l.Append(8)
+				l.Append(9)
+				l.Append(10)
+				l.Append(14)
+				l.Append(12)
+				return l
+			},
+			want: "1 8 6 4 2 9 12 14 10 ",
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			l := tt.prepare()
+			l.ReverseEven()
+
+			buf := bytes.Buffer{}
+			l.Print(&buf)
+			if got := buf.String(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("want %v, but got %v", tt.want, got)
+			}
+		})
+	}
 }
