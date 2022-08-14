332114737fad6940df19bb4c6ca19dce84098500
aoba <ipbdipbd00@gmail.com>
2022-08-14 11:58:57 +0900

    Add prime of quize

--- quiz/make_prime.go
+++ quiz/make_prime.go
@@ -1,24 +1,17 @@
 package quiz
 
-func isPrime(n int) bool {
-	if n <= 1 {
-		return false
-	}
+import "math"
 
-	for i := 2; i < n; i++ {
-		if n%i == 0 {
-			return false
-		}
-	}
-	return true
-}
+// -----------------------------
+// 素数抜き出し問題
+// Input: 50 =>	 Output: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
 
 // 冗長な方法
 func generatePrimesV1(n int) []int {
 	primes := []int{}
 
 	for i := 2; i <= n; i++ {
-		if isPrime(i) {
+		if isPrimeV1(i) {
 			primes = append(primes, i)
 		}
 	}
@@ -46,4 +39,108 @@ func generatePrimesV2(n int) []int {
 	}
 
 	return primes
+}
+
+func generateSliceWithDefaultVal[T any](n int, val T) []T {
+	result := make([]T, n)
+	for i := range result {
+		result[i] = val
+	}
+	return result
+}
+
+// エラトステネスの篩を使用する方法
+func generatePrimesV3(n int) []int {
+	if n <= 1 {
+		return []int{}
+	}
+
+	cache := generateSliceWithDefaultVal(n, true)
+	cache[0], cache[1] = false, false
+
+	for current := 2; current < n; current++ {
+		if !cache[current] {
+			continue
+		}
+
+		i := 2
+		j := current * 2
+		for j < n {
+			cache[j] = false
+			i++
+			j = current * i
+		}
+	}
+
+	primes := []int{}
+	for i := 2; i < len(cache); i++ {
+		if cache[i] {
+			primes = append(primes, i)
+		}
+	}
+
+	return primes
+}
+
+// -----------------------------
+// 素数判定問題
+
+// 冗長な方法
+func isPrimeV1(n int) bool {
+	if n <= 1 {
+		return false
+	}
+
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// √をつかった方法
+func isPrimeV2(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	if n == 2 {
+		return true
+	}
+	if n%2 == 0 {
+		return false
+	}
+
+	lessNum := int(math.Sqrt(float64(n))) + 1
+	for i := 3; i < lessNum; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+// √を使わずにループする方法
+func isPrimeV3(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	if n == 2 {
+		return true
+	}
+	if n%2 == 0 || n%3 == 3 {
+		return false
+	}
+
+	i := 3
+	for i*i <= n {
+		if n%i == 0 {
+			return false
+		}
+
+		i++
+	}
+
+	return true
 }
--- quiz/make_prime_test.go
+++ quiz/make_prime_test.go
@@ -5,8 +5,6 @@ import (
 	"testing"
 )
 
-// Input: 50 => Output: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
-
 func Test_generatePrimesV1(t *testing.T) {
 	type args struct {
 		n int
@@ -53,4 +51,106 @@ func Test_generatePrimesV2(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_generatePrimesV3(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "Case1",
+			args: args{50},
+			want: []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := generatePrimesV3(tt.args.n); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("generatePrimesV3() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_isPrimeV1(t *testing.T) {
+	t.Run("test is prime ok", func(t *testing.T) {
+		for _, n := range []int{2, 3, 5, 7, 11, 13, 17, 19} {
+			if isPrimeV1(n) == false {
+				t.Errorf("want true, but want false")
+			}
+		}
+	})
+
+	t.Run("test is prime no", func(t *testing.T) {
+		for _, n := range []int{1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20} {
+			if isPrimeV1(n) == true {
+				t.Errorf("want false, but want true")
+			}
+		}
+	})
+
+	t.Run("test is prime no, invalid number", func(t *testing.T) {
+		for _, n := range []int{-1} {
+			if isPrimeV1(n) == true {
+				t.Errorf("want false, but want true")
+			}
+		}
+	})
+}
+
+func Test_isPrimeV2(t *testing.T) {
+	t.Run("test is prime ok", func(t *testing.T) {
+		for _, n := range []int{2, 3, 5, 7, 11, 13, 17, 19} {
+			if isPrimeV2(n) == false {
+				t.Errorf("isPrimeV2(%v), want true, but want false", n)
+			}
+		}
+	})
+
+	t.Run("test is prime no", func(t *testing.T) {
+		for _, n := range []int{1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20} {
+			if isPrimeV2(n) == true {
+				t.Errorf("isPrimeV2(%v), want false, but want true", n)
+			}
+		}
+	})
+
+	t.Run("test is prime no, invalid number", func(t *testing.T) {
+		for _, n := range []int{-1} {
+			if isPrimeV2(n) == true {
+				t.Errorf("isPrimeV2(%v), want false, but want true", n)
+			}
+		}
+	})
+}
+
+func Test_isPrimeV3(t *testing.T) {
+	t.Run("test is prime ok", func(t *testing.T) {
+		for _, n := range []int{2, 3, 5, 7, 11, 13, 17, 19} {
+			if isPrimeV3(n) == false {
+				t.Errorf("isPrimeV3(%v), want true, but want false", n)
+			}
+		}
+	})
+
+	t.Run("test is prime no", func(t *testing.T) {
+		for _, n := range []int{1, 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20} {
+			if isPrimeV3(n) == true {
+				t.Errorf("isPrimeV3(%v), want false, but want true", n)
+			}
+		}
+	})
+
+	t.Run("test is prime no, invalid number", func(t *testing.T) {
+		for _, n := range []int{-1} {
+			if isPrimeV3(n) == true {
+				t.Errorf("isPrimeV3(%v), want false, but want true", n)
+			}
+		}
+	})
 }
