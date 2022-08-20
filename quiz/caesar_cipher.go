58d7f2cc847db016d8b05f895744d99092d1408c
aoba <ipbdipbd00@gmail.com>
2022-08-20 09:46:33 +0900

    Add caesar chiper of quiz

--- quiz/caesar_cipher.go
+++ quiz/caesar_cipher.go
@@ -12,8 +12,8 @@ var lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz"
 
 var ErrInvalidText = errors.New("invalid text error")
 
-// alphabetの配列を使う方法
-func caesarCipherV1(text string, shift int) (string, error) {
+// alphabetの文字列を使う方法
+func caesarCipherV1(text string, shift int) string {
 	result := ""
 
 	for _, char := range text {
@@ -42,19 +42,94 @@ func caesarCipherV1(text string, shift int) (string, error) {
 
 	}
 
-	return result, nil
+	return result
 }
 
+// rune(code point)
+// a-z: 97-122
+// A-Z: 65-90
+
 // unicode pointを使う方法
-func caesarCipherV2(text string, shift int) (string, error) {
-	// s := ""
-	for _, char := range text {
-		if string(char) == " " {
+func caesarCipherV2(text string, shift int) string {
+	result := ""
+	lenAlphabet := 'Z' - 'A' + 1
 
+	for _, charRune := range text {
+		if unicode.IsUpper(charRune) {
+			newCharRune := (charRune+rune(shift)-'A')%lenAlphabet + 'A'
+			if newCharRune < 'A' {
+				result += string(newCharRune + rune(lenAlphabet))
+			} else {
+				result += string(newCharRune)
+			}
+		} else if unicode.IsLower(charRune) {
+			newCharRune := (charRune+rune(shift)-'a')%lenAlphabet + 'a'
+			if newCharRune < 'a' {
+				result += string(newCharRune + rune(lenAlphabet))
+			} else {
+				result += string(newCharRune)
+			}
 		} else {
-
+			result += string(charRune)
 		}
 	}
 
-	return "", nil
+	return result
+}
+
+// alphabetの文字列を使う方法
+func caesarCipherHackV1(text string) []string {
+	lenAlphabet := len(uppercaseAlphabet)
+	result := make([]string, 0, lenAlphabet)
+
+	for i := range make([]struct{}, lenAlphabet) {
+		incrementCount := i + 1
+		tmp := ""
+
+		for _, charRune := range text {
+			var alphabet string
+			if unicode.IsUpper(charRune) {
+				alphabet = uppercaseAlphabet
+			} else if unicode.IsLower(charRune) {
+				alphabet = lowercaseAlphabet
+			} else {
+				tmp += string(charRune)
+				continue
+			}
+
+			index := strings.IndexRune(alphabet, charRune)
+			newIndex := (index + incrementCount) % len(alphabet)
+			tmp += string(alphabet[newIndex])
+		}
+
+		result = append(result, tmp)
+	}
+
+	return result
+}
+
+// unicode pointを使う方法
+func caesarCipherHackV2(text string) []string {
+	lenAlphabet := int('Z' - 'A' + 1)
+	result := make([]string, 0, lenAlphabet)
+
+	for i := 1; i <= lenAlphabet; i++ {
+		tmp := ""
+
+		for _, charRune := range text {
+			if unicode.IsUpper(charRune) {
+				newRune := (charRune-'A'+rune(i))%rune(lenAlphabet) + 'A'
+				tmp += string(newRune)
+			} else if unicode.IsLower(charRune) {
+				newRune := (charRune-'a'+rune(i))%rune(lenAlphabet) + 'a'
+				tmp += string(newRune)
+			} else {
+				tmp += string(charRune)
+			}
+		}
+
+		result = append(result, tmp)
+	}
+
+	return result
 }
--- quiz/caesar_cipher_test.go
+++ quiz/caesar_cipher_test.go
@@ -8,14 +8,35 @@ import (
 func Test_caesarCipherV1(t *testing.T) {
 	text := "ATTACK SILICON VALLEY by engineer"
 
-	ret, err := caesarCipherV1(text, 3)
-	if err != nil {
-		t.Fatalf("unexpected error, err: %v", err)
-	}
-
+	ret := caesarCipherV1(text, 3)
 	fmt.Printf("ret: %v\n", ret)
 
-	ret2, _ := caesarCipherV1(ret, -3)
-
+	ret2 := caesarCipherV1(ret, -3)
 	fmt.Printf("ret2: %v\n", ret2)
+}
+
+func Test_caesarCipherV2(t *testing.T) {
+	text := "ATTACK SILICON VALLEY by engineer"
+
+	ret := caesarCipherV2(text, 3)
+	fmt.Printf("ret: %v\n", ret)
+
+	ret2 := caesarCipherV2(ret, -3)
+	fmt.Printf("ret2: %v\n", ret2)
+}
+
+func Test_caesarCipherHackV1(t *testing.T) {
+	ret := caesarCipherHackV1("DWWDFN VLOLFRQ YDOOHB eb hqjlqhhu")
+
+	for i, t := range ret {
+		fmt.Printf("[%d] %s\n", i, t)
+	}
+}
+
+func Test_caesarCipherHackV2(t *testing.T) {
+	ret := caesarCipherHackV2("DWWDFN VLOLFRQ YDOOHB eb hqjlqhhu")
+
+	for i, t := range ret {
+		fmt.Printf("[%d] %s\n", i, t)
+	}
 }
