bfaeaff9e0a91b84a3bbab49a8badd995a46ebb0
aoba <ipbdipbd00@gmail.com>
2022-09-17 17:09:50 +0900

    Add _template_with_files

---
+++ math_algo_book/_template_with_files/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+var reader io.Reader
+var writer io.Writer
+
+func init() {
+	reader = os.Stdin
+	writer = os.Stdout
+}
+
+func main() {
+	sc := bufio.NewScanner(reader)
+	sc.Split(bufio.ScanWords)
+	n := ni(sc)
+	a := nis(sc, n)
+
+	answer := 0
+	for _, num := range a {
+		answer += num
+	}
+
+	fmt.Fprint(writer, answer)
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ni2(sc *bufio.Scanner) (int, int) {
+	return ni(sc), ni(sc)
+}
+
+func ni3(sc *bufio.Scanner) (int, int, int) {
+	return ni(sc), ni(sc), ni(sc)
+}
+
+func ni4(sc *bufio.Scanner) (int, int, int, int) {
+	return ni(sc), ni(sc), ni(sc), ni(sc)
+}
+
+func nis(sc *bufio.Scanner, n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni(sc)
+	}
+	return a
+}
