8a53593823edbf76b3e5bb8c1b1658fb3922abb9
aoba <ipbdipbd00@gmail.com>
2022-10-23 13:09:09 +0900

    TBD: id_036

---
+++ math_algo_book/id_036/main_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  string
+	}{
+		{
+			name:  "case1",
+			input: "3 4 9 0",
+			want:  "5.00000000000000000000",
+		},
+		// {
+		// 	name:  "case2",
+		// 	input: "3 4 10 40",
+		// 	want:  "4.56425719433005567605",
+		// },
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// set up reader and writer
+			input := &bytes.Buffer{}
+			_, err := input.WriteString(test.input)
+			if err != nil {
+				t.Fatalf("failed to setup input data, err: %v", err)
+			}
+			reader = input
+
+			result := &bytes.Buffer{}
+			writer = result
+
+			// execution
+			main()
+
+			// assertion
+			if test.want != result.String() {
+				t.Errorf("want: %v, but got: %v", test.want, result.String())
+			}
+		})
+	}
+}
