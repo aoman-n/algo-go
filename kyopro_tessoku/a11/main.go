6b52cb66caedbdaf1157ea5af6ba61a082bdf12d
aoman <ipbdipbd00@gmail.com>
2022-12-29 21:50:13 +0900

    add a11

---
+++ kyopro_tessoku/a11/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+var reader io.Reader
+var writer io.Writer
+
+const MOD = 1000000007
+
+func init() {
+	reader = os.Stdin
+	writer = os.Stdout
+}
+
+func main() {
+	sc := bufio.NewScanner(reader)
+	sc.Split(bufio.ScanWords)
+
+	// N=要素数
+	// X=検索ターゲット
+	N, X := ni2(sc)
+
+	A := make([]int, N)
+	for i := range A {
+		A[i] = ni(sc)
+	}
+
+	fmt.Fprint(writer, search(X, A))
+}
+
+func search(target int, a []int) int {
+	l, r := 0, len(a)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if a[mid] == target {
+			return mid + 1
+		}
+
+		if target < a[mid] {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+
+	return -1
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ni2(sc *bufio.Scanner) (int, int) {
+	return ni(sc), ni(sc)
+}
+
+func ni3(sc *bufio.Scanner) (int, int, int) {
+	return ni(sc), ni(sc), ni(sc)
+}
+
+func ni4(sc *bufio.Scanner) (int, int, int, int) {
+	return ni(sc), ni(sc), ni(sc), ni(sc)
+}
+
+func nis(sc *bufio.Scanner, n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni(sc)
+	}
+	return a
+}
+
+// n: 行数
+// m: 要素数
+func nir(sc *bufio.Scanner, n int, m int) [][]int {
+	a := make([][]int, n)
+	for i := range a {
+		a[i] = nis(sc, m)
+	}
+	return a
+}
+
+// ==================================================
+// util
+// ==================================================
+
+func abs(n int) int {
+	return int(math.Abs(float64(n)))
+}
