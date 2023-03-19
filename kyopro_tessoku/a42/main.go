ea6fa381ef115267dbd600e4f135a1190a95ea2d
aoman <ipbdipbd00@gmail.com>
2023-03-19 17:12:44 +0900

    add a42

---
+++ kyopro_tessoku/a42/main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+var reader io.Reader
+var writer io.Writer
+
+const (
+	MOD = 1000000007
+	// ↓AtCoderがGo1.14.1を使用しているため、定義しておく
+	intSize = 32 << (^uint(0) >> 63) // 32 or 64
+	MaxInt  = 1<<(intSize-1) - 1
+	MinInt  = -1 << (intSize - 1)
+)
+
+func init() {
+	reader = os.Stdin
+	writer = os.Stdout
+}
+
+func main() {
+	sc := bufio.NewScanner(reader)
+	sc.Split(bufio.ScanWords)
+	// 長い行を読み込めるようにするための定義
+	sc.Buffer([]byte{}, MaxInt)
+	N, K := ni2(sc)
+
+	// A=体力
+	// B=気力
+	A := make([]int, N)
+	B := make([]int, N)
+
+	for i := 0; i < N; i++ {
+		A[i], B[i] = ni2(sc)
+	}
+
+	isMatchedFn := func(a, b, minA, minB int) bool {
+		return a >= minA && a <= minA+K && // aの範囲を検証
+			b >= minB && b <= minB+K // bの範囲を検証
+	}
+
+	// func(x=下限体力/y=下限気力)
+	getMatchedCount := func(minA, minB int) int {
+		count := 0
+		for i := 0; i < len(A); i++ {
+			a := A[i]
+			b := B[i]
+
+			if isMatchedFn(a, b, minA, minB) {
+				count++
+			}
+		}
+
+		return count
+	}
+
+	answer := 0
+	// i = 体力の下限値
+	// j = 気力の下限値
+	for i := 1; i < 100; i++ {
+		for j := 1; j < 100; j++ {
+			count := getMatchedCount(i, j)
+			if count > answer {
+				answer = count
+			}
+		}
+	}
+
+	fmt.Fprint(writer, answer)
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ns(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func ni2(sc *bufio.Scanner) (int, int) {
+	return ni(sc), ni(sc)
+}
+
+func ni3(sc *bufio.Scanner) (int, int, int) {
+	return ni(sc), ni(sc), ni(sc)
+}
+
+func ni4(sc *bufio.Scanner) (int, int, int, int) {
+	return ni(sc), ni(sc), ni(sc), ni(sc)
+}
+
+func nis(sc *bufio.Scanner, n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni(sc)
+	}
+	return a
+}
+
+// n: 行数
+// m: 要素数
+func nir(sc *bufio.Scanner, n int, m int) [][]int {
+	a := make([][]int, n)
+	for i := range a {
+		a[i] = nis(sc, m)
+	}
+	return a
+}
+
+// ==================================================
+// util
+// ==================================================
+
+func abs(n int) int {
+	return int(math.Abs(float64(n)))
+}
+
+func min(a, b int) int {
+	return int(math.Min(float64(a), float64(b)))
+}
+
+func max(a, b int) int {
+	return int(math.Max(float64(a), float64(b)))
+}
+
+func sumInts(s []int) int {
+	var ret int
+	for _, v := range s {
+		ret += v
+	}
+	return ret
+}
+
+func maxInt(s []int) int {
+	if len(s) <= 0 {
+		return MinInt
+	}
+
+	max := s[0]
+	for i := 1; i < len(s); i++ {
+		if s[i] > max {
+			max = s[i]
+		}
+	}
+
+	return max
+}
