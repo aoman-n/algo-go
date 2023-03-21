3673134a375fabd9dbcb23c94ecb5dd51a26b77b
aoman <ipbdipbd00@gmail.com>
2023-03-21 12:04:37 +0900

    add b44

---
+++ kyopro_tessoku/b44/main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+var reader io.Reader
+var writer io.Writer
+
+const (
+	MOD = 1000000007
+	// ↓AtCoderがGo1.14.1を使用しているため、定義しておく
+	intSize = 32 << (^uint(0) >> 63) // 32 or 64
+	MaxInt  = 1<<(intSize-1) - 1
+	MinInt  = -1 << (intSize - 1)
+)
+
+func init() {
+	reader = os.Stdin
+	writer = os.Stdout
+}
+
+func main() {
+	sc := bufio.NewScanner(reader)
+	sc.Split(bufio.ScanWords)
+	// 長い行を読み込めるようにするための定義
+	sc.Buffer([]byte{}, MaxInt)
+
+	// N*Nの表
+	N := ni(sc)
+
+	/*
+		N=3のとき
+		[
+			[1, 2, 3],
+			[1, 2, 3],
+			[1, 2, 3],
+		]
+	*/
+	A := make([][]int, N)
+	for i := range A {
+		A[i] = nis(sc, N)
+	}
+
+	// Q=操作回数
+	Q := ni(sc)
+
+	// 1: 交換操作：整数 x,y が与えられるので、x 行目と y 行目を交換する
+	// 2: 取得操作：整数 x,y が与えられるので、マス (x,y) に書かれた整数を答える
+	for range make([]struct{}, Q) {
+		ope, x, y := ni3(sc)
+
+		switch ope {
+		case 1: // 交換操作
+			A[x-1], A[y-1] = A[y-1], A[x-1]
+		case 2: // 取得操作
+			fmt.Fprintln(writer, A[x-1][y-1])
+		}
+	}
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ns(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func ni2(sc *bufio.Scanner) (int, int) {
+	return ni(sc), ni(sc)
+}
+
+func ni3(sc *bufio.Scanner) (int, int, int) {
+	return ni(sc), ni(sc), ni(sc)
+}
+
+func ni4(sc *bufio.Scanner) (int, int, int, int) {
+	return ni(sc), ni(sc), ni(sc), ni(sc)
+}
+
+func nis(sc *bufio.Scanner, n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni(sc)
+	}
+	return a
+}
+
+// n: 行数
+// m: 要素数
+func nir(sc *bufio.Scanner, n int, m int) [][]int {
+	a := make([][]int, n)
+	for i := range a {
+		a[i] = nis(sc, m)
+	}
+	return a
+}
+
+// ==================================================
+// util
+// ==================================================
+
+func abs(n int) int {
+	return int(math.Abs(float64(n)))
+}
+
+func min(a, b int) int {
+	return int(math.Min(float64(a), float64(b)))
+}
+
+func max(a, b int) int {
+	return int(math.Max(float64(a), float64(b)))
+}
+
+func sumInts(s []int) int {
+	var ret int
+	for _, v := range s {
+		ret += v
+	}
+	return ret
+}
+
+func maxInt(s []int) int {
+	if len(s) <= 0 {
+		return MinInt
+	}
+
+	max := s[0]
+	for i := 1; i < len(s); i++ {
+		if s[i] > max {
+			max = s[i]
+		}
+	}
+
+	return max
+}
