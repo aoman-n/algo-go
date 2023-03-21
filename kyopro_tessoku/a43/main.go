1d70b112f96850e40de17a091fc7fb13d97fcdfa
aoman <ipbdipbd00@gmail.com>
2023-03-21 10:39:53 +0900

    add a43

---
+++ kyopro_tessoku/a43/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+var reader io.Reader
+var writer io.Writer
+
+const (
+	MOD = 1000000007
+	// ↓AtCoderがGo1.14.1を使用しているため、定義しておく
+	intSize = 32 << (^uint(0) >> 63) // 32 or 64
+	MaxInt  = 1<<(intSize-1) - 1
+	MinInt  = -1 << (intSize - 1)
+)
+
+func init() {
+	reader = os.Stdin
+	writer = os.Stdout
+}
+
+func main() {
+	sc := bufio.NewScanner(reader)
+	sc.Split(bufio.ScanWords)
+	// 長い行を読み込めるようにするための定義
+	sc.Buffer([]byte{}, MaxInt)
+
+	// N=人数
+	// L=トンネルの長さ
+	N, L := ni2(sc)
+
+	// A=西端("W")からの位置
+	// B=進行方向("W"=西へ/"E"=東へ)
+	A := make([]int, N)
+	B := make([]string, N)
+	for i := range A {
+		A[i] = ni(sc)
+		B[i] = ns(sc)
+	}
+
+	// Q. 全員が秒速 1 メートルで歩くとき、最後の人がトンネルの外に出るのは何秒後
+
+	maxTime := 0
+	for i := range A {
+		a := A[i]
+		b := B[i]
+
+		switch b {
+		case "E":
+			if L-a > maxTime {
+				maxTime = L - a
+			}
+		case "W":
+			if a > maxTime {
+				maxTime = a
+			}
+		}
+	}
+
+	fmt.Fprint(writer, maxTime)
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ns(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func ni2(sc *bufio.Scanner) (int, int) {
+	return ni(sc), ni(sc)
+}
+
+func ni3(sc *bufio.Scanner) (int, int, int) {
+	return ni(sc), ni(sc), ni(sc)
+}
+
+func ni4(sc *bufio.Scanner) (int, int, int, int) {
+	return ni(sc), ni(sc), ni(sc), ni(sc)
+}
+
+func nis(sc *bufio.Scanner, n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni(sc)
+	}
+	return a
+}
+
+// n: 行数
+// m: 要素数
+func nir(sc *bufio.Scanner, n int, m int) [][]int {
+	a := make([][]int, n)
+	for i := range a {
+		a[i] = nis(sc, m)
+	}
+	return a
+}
+
+// ==================================================
+// util
+// ==================================================
+
+func abs(n int) int {
+	return int(math.Abs(float64(n)))
+}
+
+func min(a, b int) int {
+	return int(math.Min(float64(a), float64(b)))
+}
+
+func max(a, b int) int {
+	return int(math.Max(float64(a), float64(b)))
+}
+
+func sumInts(s []int) int {
+	var ret int
+	for _, v := range s {
+		ret += v
+	}
+	return ret
+}
+
+func maxInt(s []int) int {
+	if len(s) <= 0 {
+		return MinInt
+	}
+
+	max := s[0]
+	for i := 1; i < len(s); i++ {
+		if s[i] > max {
+			max = s[i]
+		}
+	}
+
+	return max
+}
