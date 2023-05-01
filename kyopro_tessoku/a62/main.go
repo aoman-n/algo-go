e0b9b9ba1982e93b8eae859fd1ebbca8ee494390
aoman <ipbdipbd00@gmail.com>
2023-05-01 10:39:58 +0900

    a62

---
+++ kyopro_tessoku/a62/main.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+)
+
+var reader io.Reader
+var writer io.Writer
+
+const (
+	MOD = 1000000007
+	// ↓AtCoderがGo1.14.1を使用しているため、定義しておく
+	intSize = 32 << (^uint(0) >> 63) // 32 or 64
+	MaxInt  = 1<<(intSize-1) - 1
+	MinInt  = -1 << (intSize - 1)
+)
+
+func init() {
+	reader = os.Stdin
+	writer = os.Stdout
+}
+
+func isConnected(graphList [][]int) bool {
+	passed := walk(graphList)
+
+	ret := true
+	for i := 1; i < len(passed); i++ {
+		if !passed[i] {
+			ret = false
+			break
+		}
+	}
+
+	return ret
+}
+
+// walk は各頂点を通ったかのbool値を持つ配列を返す
+// 配列のindex=頂点番号
+func walk(graphList [][]int) []bool {
+	ret := make([]bool, len(graphList))
+	walkHelper(graphList, 1, ret)
+	return ret
+}
+
+func walkHelper(graphList [][]int, curr int, ret []bool) {
+	ret[curr] = true
+	for _, connectedVertex := range graphList[curr] {
+		if !ret[connectedVertex] {
+			walkHelper(graphList, connectedVertex, ret)
+		}
+	}
+}
+
+func main() {
+	sc := bufio.NewScanner(reader)
+	sc.Split(bufio.ScanWords)
+	// 長い行を読み込めるようにするための定義
+	sc.Buffer([]byte{}, MaxInt)
+
+	// N:頂点の数
+	// M:辺の数
+	N, M := ni2(sc)
+
+	graphList := make([][]int, N+1)
+
+	for range make([]struct{}, M) {
+		x, y := ni2(sc)
+		graphList[x] = append(graphList[x], y)
+		graphList[y] = append(graphList[y], x)
+	}
+
+	if isConnected(graphList) {
+		fmt.Fprintln(writer, "The graph is connected.")
+	} else {
+		fmt.Fprintln(writer, "The graph is not connected.")
+	}
+}
+
+// ==================================================
+// io
+// ==================================================
+
+func ni(sc *bufio.Scanner) int {
+	sc.Scan()
+	i, e := strconv.Atoi(sc.Text())
+	if e != nil {
+		panic(e)
+	}
+	return i
+}
+
+func ns(sc *bufio.Scanner) string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func ni2(sc *bufio.Scanner) (int, int) {
+	return ni(sc), ni(sc)
+}
+
+func ni3(sc *bufio.Scanner) (int, int, int) {
+	return ni(sc), ni(sc), ni(sc)
+}
+
+func ni4(sc *bufio.Scanner) (int, int, int, int) {
+	return ni(sc), ni(sc), ni(sc), ni(sc)
+}
+
+func nis(sc *bufio.Scanner, n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = ni(sc)
+	}
+	return a
+}
+
+// n: 行数
+// m: 要素数
+func nir(sc *bufio.Scanner, n int, m int) [][]int {
+	a := make([][]int, n)
+	for i := range a {
+		a[i] = nis(sc, m)
+	}
+	return a
+}
+
+// ==================================================
+// util
+// ==================================================
+
+func abs(n int) int {
+	return int(math.Abs(float64(n)))
+}
+
+func min(a, b int) int {
+	return int(math.Min(float64(a), float64(b)))
+}
+
+func max(a, b int) int {
+	return int(math.Max(float64(a), float64(b)))
+}
+
+func sumInts(s []int) int {
+	var ret int
+	for _, v := range s {
+		ret += v
+	}
+	return ret
+}
+
+func maxInt(s []int) int {
+	if len(s) <= 0 {
+		return MinInt
+	}
+
+	max := s[0]
+	for i := 1; i < len(s); i++ {
+		if s[i] > max {
+			max = s[i]
+		}
+	}
+
+	return max
+}
+
+// 見つかった場合対象のindexを返す
+func Search(s []int, v int) (int, bool) {
+	if len(s) <= 0 {
+		return -1, false
+	}
+
+	return searchHelper(s, v, 0, len(s)-1)
+}
+
+func searchHelper(s []int, v, l, r int) (int, bool) {
+	if l > r {
+		return -1, false
+	}
+
+	mid := (l + r) / 2
+
+	if s[mid] == v {
+		return mid, true
+	}
+
+	if v < s[mid] {
+		return searchHelper(s, v, l, mid-1)
+	} else {
+		return searchHelper(s, v, mid+1, r)
+	}
+}
